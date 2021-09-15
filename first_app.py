3aee14dd1fb4ea947cd783dad47a6e7d3af8a30d
Pablo Lawson <59547313+pablolawson@users.noreply.github.com>
2021-09-15 15:59:01 -0300

    Update first_app.py

--- first_app.py
+++ first_app.py
@@ -11,8 +11,8 @@ st.write("Buscador de ranking:")
 
 
 keyword = st.text_input("KEYWORD", '')
-
-api_search = f'https://api.mercadolibre.com/MLA/search?q={keyword}' 
+site = st.text_input("site", "MLA")
+api_search = f'https://api.mercadolibre.com/sites/{site}/search?q={keyword}' 
 
 for keyword in keyword:
     data = []
